740f30b43c5dce491bcee43cb93709c64d10e0a6
CircleCI <syseng@enjoy.com>
2020-07-11 19:46:54 +0530

    Some changes in custom response parser to fetch data from job posts too.

--- lib/cvcost_custom_parser.rb
+++ lib/cvcost_custom_parser.rb
@@ -11,11 +11,25 @@ class CvcostCustomParser < Faraday::Response::Middleware
   def on_complete(env)
     json = MultiJson.load(env[:body], symbolize_keys: true)
     env[:body] = {
-      data: json || {},
-      errors: json[:errors] || [],
-      metadata: json[:metadat] || {}
+      data: data(json),
+      errors: response_errors(json),
+      metadata: metadata(json)
     }
   rescue ::MultiJson::ParseError
     raise GatewayException.new(env[:body], env[:status])
+  end
+
+  private
+
+  def data(json)
+    json || (json.is_a?(Array) ? []: {})
+  end
+
+  def response_errors(json)
+    json.respond_to?(:errors) ? (json[:errors] || []) : []
+  end
+
+  def metadata(json)
+    json.respond_to?(:metadata) ? (json[:metadata] || {}) : {}
   end
 end
